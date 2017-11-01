75c1222ebcb523e103b09b882ef9e9f5667c81f3
Kavya sri Rachamallu <kavyasri563@gmail.com>
2017-11-01 12:02:39 -0400

    Update Fooed.java

--- src/com/test/Fooed.java
+++ src/com/test/Fooed.java
@@ -7,34 +7,34 @@ import java.util.LinkedHashMap;
  * @see Calculator
  * @see Order
  */
-public class Fooed {
+public class Foo {
 	public static void main(String[] args) throws Exception {
 
 		LinkedHashMap<String, Order> orders = new LinkedHashMap<String, Order>();
 
 		Order order = new Order();
 
-		order.add(new OrderLine(new Item("book", (float) 13.74), 1));
-		order.add(new OrderLine(new Item("music CD", (float) 16.49), 1));
-		order.add(new OrderLine(new Item("chocolate bar", (float) 0.94), 1));
+		order.add(new OrderLine(new Item("book", (float) 12.49), 1));
+		order.add(new OrderLine(new Item("music CD", (float) 14.99), 1));
+		order.add(new OrderLine(new Item("chocolate bar", (float) 0.85), 1));
 
 		orders.put("Order 1", order);
 
 		// Reuse cart for an other order
 		order = new Order();
 
-		order.add(new OrderLine(new Item("imported box of chocolate", (float)11.5), 1));
-		order.add(new OrderLine(new Item("imported bottle of perfume", (float) 54.62), 1));
+		order.add(new OrderLine(new Item("imported box of chocolate", 10), 1));
+		order.add(new OrderLine(new Item("imported bottle of perfume", (float) 47.50), 1));
 
 		orders.put("Order 2", order);
 
 		// Reuse cart for an other order
 		order = new Order();
 
-		order.add(new OrderLine(new Item("Imported bottle of perfume", (float) 32.19), 1));
-		order.add(new OrderLine(new Item("bottle of perfume", (float) 20.89), 1));
-		order.add(new OrderLine(new Item("packet of headache pills", (float) 10.73), 1));
-		order.add(new OrderLine(new Item("box of importd chocolates", (float) 12.94), 1));
+		order.add(new OrderLine(new Item("Imported bottle of perfume", (float) 27.99), 1));
+		order.add(new OrderLine(new Item("bottle of perfume", (float) 18.99), 1));
+		order.add(new OrderLine(new Item("packet of headache pills", (float) 9.75), 1));
+		order.add(new OrderLine(new Item("box of importd chocolates", (float) 11.25), 1));
 
 		orders.put("Order 3", order);
 
